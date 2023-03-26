ae272f0c568be7388099087f1f0af5f5c6935b58
varun8177 <varunergurala9999@gmail.com>
2023-03-26 23:02:18 +0530

    updated url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8080"
+                url: "https://drab-puce-tortoise-shoe.cyclic.app/"
             }
         ]
     },
